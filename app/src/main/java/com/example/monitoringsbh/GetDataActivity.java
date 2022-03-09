3060cf8fd081546dcf80e81cbf43112ef72ab6d0
arirismansyah <16.9023@stis.ac.id>
2022-03-09 13:35:31 +0700

    initial commit

---
+++ app/src/main/java/com/example/monitoringsbh/GetDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.monitoringsbh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_data);
+    }
+}
